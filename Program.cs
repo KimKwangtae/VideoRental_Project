233bcc1209c4c70a968b8e7cd00dc56a2bcc492f
gwangtae.kim <gwangtae.kim@semics.com>
2023-08-22 01:29:23 +0900

    문제2) Code Refactroing 개선점 4 수정

--- Program.cs
+++ Program.cs
@@ -7,24 +7,26 @@
     {
         static void Main(string[] args)
         {
-            Movie regular1 = new Movie("일반 1", Movie.EnumPriceCode.REGULAR);
-            Movie regular2 = new Movie( "일반 2", Movie.EnumPriceCode.REGULAR);
-            Movie newRelease1 = new Movie( "신작 1", Movie.EnumPriceCode.NEW_RELEASE);
-            Movie newRelease2 = new Movie( "신작 2",Movie.EnumPriceCode.NEW_RELEASE);
-            Movie children1 = new Movie( "어린이 1", Movie.EnumPriceCode.CHILDRENS);
-            Movie children2 = new Movie( "어린이 2", Movie.EnumPriceCode.CHILDRENS);
-            Movie exampleGenre = new Movie("새로운장르 1", Movie.EnumPriceCode.EXAMPLE_GENRE);
-            Customer customer = new Customer("고객");
+            VideoRentalShop rentalShop = new VideoRentalShop("비디오렌탈샵");
+            rentalShop.AddMovie("일반 1", Movie.EnumPriceCode.REGULAR);
+            rentalShop.AddMovie("일반 2", Movie.EnumPriceCode.REGULAR);
+            rentalShop.AddMovie("신작 1", Movie.EnumPriceCode.NEW_RELEASE);
+            rentalShop.AddMovie("신작 2", Movie.EnumPriceCode.NEW_RELEASE);
+            rentalShop.AddMovie("어린이 1", Movie.EnumPriceCode.CHILDRENS);
+            rentalShop.AddMovie("어린이 2", Movie.EnumPriceCode.CHILDRENS);
+            rentalShop.AddMovie("새로운장르 1", Movie.EnumPriceCode.EXAMPLE_GENRE);
+            rentalShop.AddCustomer("고객");
 
-            customer.AddRental(new Rental( regular1, 2 ));
-            customer.AddRental(new Rental( regular2, 3 ));
-            customer.AddRental(new Rental( newRelease1, 1 ));
-            customer.AddRental(new Rental( newRelease2, 2 ));
-            customer.AddRental(new Rental( children1, 3 ));
-            customer.AddRental(new Rental( children2, 4 ));
-            customer.AddRental(new Rental(exampleGenre, 1));
 
-            Console.Write(customer.GetStatement());
+            rentalShop.AddRental("고객", "일반 1" ,2);
+            rentalShop.AddRental("고객", "일반 2", 3);
+            rentalShop.AddRental("고객", "신작 1", 1);
+            rentalShop.AddRental("고객", "신작 2", 2);
+            rentalShop.AddRental("고객", "어린이 1", 3);
+            rentalShop.AddRental("고객", "어린이 2", 4);
+            rentalShop.AddRental("고객", "새로운장르 1", 1);
+
+            Console.Write(rentalShop.GetCustomerStatement("고객"));
         }
     }
 }
---
+++ VideoRentalShop.cs
@@ -0,0 +1,49 @@
+﻿namespace VideoRental
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class VideoRentalShop
+    {
+        public VideoRentalShop(string storeName)
+        {
+            StoreName = storeName;
+        }
+
+        private string StoreName;
+        private List<Movie> Movies = new List<Movie>();
+        private List<Customer> Customers = new List<Customer>();
+
+        public void AddMovie(string title, Movie.EnumPriceCode priceCode)
+        {
+            Movies.Add(new Movie(title, priceCode));
+        }
+        public void AddCustomer(string customerName)
+        {
+            Customers.Add(new Customer(customerName));
+        }
+        public void AddRental(string customerName,string movieTitle,int daysRented)
+        {
+            var customer=Customers.FirstOrDefault(c => c.GetName() == customerName);
+            var movie = Movies.FirstOrDefault(m => m.GetTitle() == movieTitle);
+
+            if(customer!=null && movie !=null)
+            {
+                customer.AddRental(new Rental(movie, daysRented));
+            }
+        }
+
+        public string GetCustomerStatement(string customerName)
+        {
+            string returnStatement = "";
+            var customer = Customers.FirstOrDefault(c => c.GetName() == customerName);
+            if (customer != null )
+            {
+                returnStatement= customer.GetStatement();
+            }
+            return returnStatement;
+        }
+    }
+}
